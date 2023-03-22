6d80738f40e63edbf0391a0fe0862030d4fbf4db
timadigwe <timothyadigwe2019@gmail.com>
2023-03-22 11:58:09 +0100

    responsiveness completed

---
+++ assets/index.js
@@ -0,0 +1,25 @@
+import Union from "./Union.png";
+import settings from "./settings.png";
+import Group1 from "./Group1.png";
+import todoitems from "./todoitems.png";
+import customimage from "./customimage.png";
+import invitepeople from "./invitepeople.png";
+import icon1 from "./icon1.png";
+import icon2 from "./icon2.png";
+import icon3 from "./icon3.png";
+import icon4 from "./icon4.png";
+import icon5 from "./icon5.png";
+
+export {
+  Union,
+  settings,
+  Group1,
+  todoitems,
+  customimage,
+  invitepeople,
+  icon1,
+  icon2,
+  icon3,
+  icon4,
+  icon5,
+};
